e6eb2a70e26560a3c7b404b90effae15025ac531
Pygame17 <mateobaronrestrepo@hotmail.com>
2023-08-08 20:50:09 -0500

    We add exercises for folder's set

---
+++ 1.Comprenhesion_functions_ErrorManagement/1.sets/Set_Exercises.py
@@ -0,0 +1,64 @@
+'''
+These exercises are the page w3resource and can find in the next link:
+https://www.w3resource.com/python-exercises/sets/
+'''
+def exercises_1():
+    # 1.Write a Python program to create a set.
+    h = 1
+def exercises_2():
+
+def exercises_3():
+
+def exercises_4():
+
+def exercises_5():
+
+def exercises_6():
+
+def exercises_7():
+
+def exercises_8():
+
+def exercises_9():
+
+def exercises_10():
+
+def exercises_11():
+
+def exercises_12():
+
+def exercises_13():
+
+def exercises_14():
+
+def exercises_15():
+
+def exercises_16():
+
+def exercises_17():
+
+def exercises_18():
+
+def exercises_19():
+
+def exercises_20():
+
+def exercises_21():
+
+def exercises_22():
+
+def exercises_23():
+
+def exercises_24():
+
+def exercises_25():
+
+def exercises_26():
+
+def exercises_27():
+
+def exercises_28():
+
+def exercises_29():
+
+def exercises_30():
