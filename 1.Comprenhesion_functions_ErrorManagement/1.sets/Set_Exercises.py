1e96d916bfb21720e64ef7d74d33f879933d3940
Pygame17 <mateobaronrestrepo@hotmail.com>
2023-08-10 07:57:27 -0500

    We have added a list of exercises on sets to develop them

--- 1.Comprenhesion_functions_ErrorManagement/1.sets/Set_Exercises.py
+++ 1.Comprenhesion_functions_ErrorManagement/1.sets/Set_Exercises.py
@@ -3,62 +3,94 @@ These exercises are the page w3resource and can find in the next link:
 https://www.w3resource.com/python-exercises/sets/
 '''
 def exercises_1():
-    # 1.Write a Python program to create a set.
-    h = 1
+    # 1. Write a Python program to create a set.
+    
 def exercises_2():
+    # 2. Write a search Python program to iterate over sets.
 
 def exercises_3():
+    # 3. Write a Python program to add member(s) to a set.
 
 def exercises_4():
+    #4. Write a Python program to remove item(s) from a given set.
 
 def exercises_5():
+    #5. Write a Python program to remove an item from a set if it is present in the set.
 
 def exercises_6():
+    #6. Write a Python program to create an intersection of sets.
 
 def exercises_7():
+    #7. Write a Python program to create a union of sets.
 
 def exercises_8():
+    #8. Write a Python program to create set difference.
 
 def exercises_9():
+    #9. Write a Python program to create a symmetric difference.
 
-def exercises_10():
+def exercises_10():    
+    #10. Write a search Python program to check if a set is a subset of another set.
 
 def exercises_11():
+    #11. Write a search Python program to create a shallow copy of sets.
+    #Note : Shallow copy is a bit-wise copy of an object. A new object is created that has an exact copy of the values in the original object.
 
 def exercises_12():
+    #12. Write a Python program to remove all elements from a given set.
 
 def exercises_13():
+    # 13. Write a Python program that uses frozensets.
+    #Note: Frozensets behave just like sets except they are immutable.
 
-def exercises_14():
+def exercises_14():    
+    #14. Write a Python program to find the maximum and minimum values in a set.
 
-def exercises_15():
+def exercises_15():    
+    #15. Write a search Python program to find the length of a set.
 
 def exercises_16():
+    #16. Write a Python program to check if a given value is present in a set or not.
 
 def exercises_17():
+    #17. Write a search Python program to check if two given sets have no elements in common.
 
-def exercises_18():
+def exercises_18(): 
+    #18. Write a Python program to check if a given set is a superset of itself and a superset of another given set.
 
-def exercises_19():
+def exercises_19():    
+    #19. Write a Python program to find elements in a given set that are not in another set.
 
-def exercises_20():
+def exercises_20():    
+    #20. Write a Python program to remove the intersection of a second set with a first set.
 
 def exercises_21():
+    #21. Write a search Python program to find all the unique words and count the frequency of occurrence from a given list of strings. Use Python set data type.
 
 def exercises_22():
+    #22. Write a search Python program that finds all pairs of elements in a list whose sum is equal to a given value.
 
 def exercises_23():
+    #23. Write a Python program to find the longest common prefix of all strings. Use the Python set.
 
 def exercises_24():
+    #24. Write a Python program to find the two numbers whose product is maximum among all the pairs in a given list of numbers. Use the Python set.
 
 def exercises_25():
+    #25. Given two sets of numbers, write a search Python program to find the missing numbers in the second set as compared to the first and vice versa. Use the Python set.
 
 def exercises_26():
+    #26. Write a Python program to find all the anagrams and group them together from a given list of strings. Use the search Python data type.
 
 def exercises_27():
+    #27. Write a Python program to find all the anagrams in a given list of strings and then group them together. Use the Python data type.
 
 def exercises_28():
+    #28. Write a Python program to find all the unique combinations of 3 numbers from a given list of numbers, adding up to a target number.
 
 def exercises_29():
+    #29. Write a search Python program to find the third largest number from a given list of numbers.Use the Python set data type.
+
 
 def exercises_30():
+    #30. Write a Python program to remove all duplicates from a given list of strings and return a list of unique strings. Use the search Python set data type.
